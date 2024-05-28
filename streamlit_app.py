d5a88a5dd310af315c32e22536fd0e630fab5521
Mark Mintel <101613208+markMintel@users.noreply.github.com>
2024-05-28 15:08:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ st.time_input('School time')
 st.text_area('Description')
 st.file_uploader('Upload a photo')
 st.color_picker('Choose your favorite color')
+
+st.balloons()
+st.progress(10)
+with st.spinner('Wait for it...'):    
+  time.sleep(10)
