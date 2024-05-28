c3638fa5698884bb8d33d6a45f0e369ffdbba0aa
Mark Mintel <101613208+markMintel@users.noreply.github.com>
2024-05-28 12:05:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,6 @@ st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-st.image("streamlit_tp.png")
+show_image = st.checkbox("Show Image")
+if show_image:
+  st.image("streamlit_tp.png")
