d9bc31f5298483624db52fa11e174f0b1aabf283
Mark Mintel <101613208+markMintel@users.noreply.github.com>
2024-05-28 12:10:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,11 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 show_image = st.checkbox("Show Image")
 if show_image:
   st.image("streamlit_tp.png")
+
+
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,50)
